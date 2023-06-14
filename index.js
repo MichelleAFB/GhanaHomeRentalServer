00b73fa1242e2f3990432c14396e0ec7900bff5f
MichelleAFB <64324539+MichelleAFB@users.noreply.github.com>
2023-06-14 15:12:15 -0500

    fdsfs

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import session from 'express-session'
 */
 
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config/.env" });
+dotenv.config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
